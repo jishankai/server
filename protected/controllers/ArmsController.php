6e302033a216a32b9f6b3644751d200c5737472b
skji <jishankai@gmail.com>
2014-02-03 06:26:56 +0900

    WeaponsController&ArmsController

---
+++ protected/controllers/ArmsController.php
@@ -0,0 +1,31 @@
+<?php
+
+class ArmsController extends Controller
+{
+    public function filters()
+    {
+        return array(
+            'checkUpdate',
+            'getPlayerId',
+            //'checkSig',
+        );      
+    }
+
+    public function actionBuyApi($armId)
+    {
+        $arm = MArms::model()->findByAttributes(array( 'playerId'=>$this->playerId, 'armId'->$armId));
+        if (isset($arm)) {
+            $arm->level++;
+            $arm->saveAttributes(array('level'));
+        } else {
+            $arms = new MArms;
+            $arms->playerId = $this->playerId;
+            $arms->armId = $armId;
+            $arms->level = 1;
+            $arms->createTime = now();
+            $arms->save();
+        }
+
+        $this->echoJsonData(array('result'=>true));
+    }
+}
