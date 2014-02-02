3ba3277d45d585bdf7a11d2ee7e4a4ad50674118
skji <jishankai@gmail.com>
2014-02-03 06:13:12 +0900

    ArmsController&WeaponsController

---
+++ protected/controllers/WeaponsController.php
@@ -0,0 +1,31 @@
+<?php
+
+class WeaponsController extends Controller
+{
+    public function filters()
+    {
+        return array(
+            'checkUpdate',
+            'getPlayerId',
+            //'checkSig',
+        );      
+    }
+
+    public function actionWeaponsApi($weaponId)
+    {
+        $weapon = MWeapons::model()->findByAttributes(array('playerId'=>$this->playerId, 'weaponId'->$weaponId));
+        if (isset($weapon)) {
+            $weapon->level++;
+            $weapon->saveAttributes(array('level'));
+        } else {
+            $weapon = new MWeapon;
+            $weapon->playerId = $this->playerId;
+            $weapon->weaponid = $weaponid;
+            $weapon->level = 1;
+            $weapon->createTime = now();
+            $weapon->save();
+        }
+
+        $this->echoJsonData(array('result'=>true));
+    }
+}
